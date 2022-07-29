74c2b3604fee99949b3312f28d93996bed22eb09
rifaturrana <smrifaturrana@gmail.com>
2022-07-29 16:44:24 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+dotenv.config({ path: __dirname + "/.env" });
+app.use(cors());
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/history", require("./api/route"));
+
+const PORT = process.env.PORT || 4444;
+app.listen(PORT, () => {
+  console.log("APP is Running on PORT " + PORT);
+  mongoose.connect(
+    "mongodb+srv://rana:rana@cluster0.un1htee.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true },
+    () => {
+      console.log("Database Connected");
+    }
+  );
+});
